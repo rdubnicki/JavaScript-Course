9e526b3daaf588e1b4a207c72949af39818f34d4
Robert Dubnicki <robert.dubnicki@outlook.com>
2023-10-20 08:41:58 +0200

    Last modules of Functions, Scope & Execution Context Chapter.

---
+++ 03-functions-scope-execution-context/10-execution-context/script.js
@@ -0,0 +1,12 @@
+let x = 100;
+let y = 50;
+
+function getSum(n1, n2) {
+    let sum = n1 + n2;
+    return sum;
+}
+
+let sum1 = getSum(x, y);
+let sum2 = getSum(10, 5);
+
+console.log(sum1, sum2);
