d7c846b83e2a70f3455f24a194ad0dd76ad7a582
Robert Dubnicki <robert.dubnicki@outlook.com>
2023-11-07 15:40:43 +0100

    Start of DOM Module

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,14 @@
+// console.log(document.body.innerHTML);
+
+// console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>HELLO WORLD!</h1>';
+
+// document.write('Hello from JS');
+
+console.log(document.getElementById('main'));
+
+const main = document.getElementById('main');
+// main.innerHTML = '<h1>Hello from main</h1>';
+
+document.querySelector('#main h1').innerText = 'Hello';
